7a2ecd251f46537b845c5ed3f66a2164eb1ed5b4
Syeseong <sy051398@naver.com>
2024-05-19 00:00:08 +0900

    feat : 24-05-19 - 연락처 영역

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            Contact
-        </div>
+        <section id="contact">
+            <div class="contact__inner">
+                <h2 class="contact__title">Contact</h2>
+                <div class="contact__lines" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+                <div class="contact__text">
+                    <div class="text">
+                        <div>
+                            <a href="#">KAKAO : ...</a>
+                        </div>
+                        <div>
+                            <a href="#">mail : ...@NAVER.COM</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact__lines bottom" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+            </div>
+        </section>
     )
 }
 
